1d964d010f3bb40c645a874b68357aae497baeb4
amin <aungkhantmin.dev@gmail.com>
2025-07-07 20:32:21 +0300

    chat session cleanup

--- backend/pkg/chat/session.go
+++ backend/pkg/chat/session.go
@@ -1,60 +1,21 @@
 package chat
 
 import (
-	"database/sql"
-	"errors"
-	"log"
 	"social-network/backend/pkg/db/sqlite"
 	"social-network/backend/pkg/models"
 )
 
 func CurrentUser(path, val string) (models.User, error) {
-	query, err := sqlite.GetDB().Query(`
+	row := sqlite.GetDB().QueryRow(`
 		SELECT users.id, users.nickname, users.first_name, users.last_name, users.gender, users.email 
 		FROM sessions 
 		INNER JOIN users ON sessions.user_id = users.id 
 		WHERE sessions.token = ?`, val)
+
+	var user models.User
+	err := row.Scan(&user.ID, &user.Nickname, &user.FirstName, &user.LastName, &user.Gender, &user.Email)
 	if err != nil {
 		return models.User{}, err
 	}
-	defer query.Close()
-
-	users, err := ConvertRowToUser(query)
-	if err != nil {
-		return models.User{}, err
-	}
-
-	if len(users) == 0 {
-		return models.User{}, errors.New("no users found")
-	}
-
-	return users[0], nil
-}
-
-// Convert the database row into a user struct
-func ConvertRowToUser(rows *sql.Rows) ([]models.User, error) {
-	var users []models.User
-
-	for rows.Next() {
-		var user models.User
-
-		// Store the row data in the temporary user struct
-		err := rows.Scan(
-			&user.ID,
-			&user.Nickname,
-			&user.FirstName,
-			&user.LastName,
-			&user.Gender,
-			&user.Email)
-
-		if err != nil {
-			log.Printf("Row scan error: %v", err)
-			break
-		}
-
-		// Append the temporary user struct to the users slice
-		users = append(users, user)
-	}
-
-	return users, nil
+	return user, nil
 }
